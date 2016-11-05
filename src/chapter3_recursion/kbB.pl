06f4e861405e35377dd374807fad142b34564402
frnmst/Franco Masotti <franco.masotti@live.com>
2016-11-05 22:43:50 +0100

    Fixed one exercise.

--- src/chapter3_recursion/kbB.pl
+++ src/chapter3_recursion/kbB.pl
@@ -11,34 +11,6 @@
  * work. If not, see <http://creativecommons.org/licenses/by-sa/4.0/>.
  */
 
-/* Binary trees. */
-
-/*
- * SOLVE THIS. 
- *
- */
-
-leaf(1).
-leaf(2).
-leaf(3).
-tree(leaf(1),leaf(2)).
-
-/* Tree base case: create a new tree from 2 vertices. */
-tree(leaf(B1),leaf(B2)).
-
-/* Thanks to unification all cases are covered by 
- * a single predicate.
- */
-tree(tree(B1),B2) :-
-    tree(B1,B2).
-
-run_tree :-
-    tree(tree(leaf(23),leaf(24)),tree(leaf(21),leaf(31))).
-
-/*
-swap(X,Y) :-
-    tree(leaf(X),leaf(Z)).
-
-swap(tree(X),T) :-
-    swap(X,T).
-*/
+If you type the question on the Internet, solutions
+are quite controversial. It seems that it might be too early,
+given the current knowledge, to implement this correcly.
